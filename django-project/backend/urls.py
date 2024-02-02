5490a64d2f0dd9eefa33a26fb2f02bc341343080
Samuel Tóth <mr.samueltoth@gmail.com>
2024-02-02 16:27:12 +0100

    feat: add URL to get all data

--- django-project/backend/urls.py
+++ django-project/backend/urls.py
@@ -4,9 +4,10 @@ from rest_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', views.UserRegister),
-    path('api/login/', views.UserLogin),
-    path('api/logout/', views.UserLogout),
-    path('api/mqtt/data/', views.MQTTData),
-    path('api/mqtt/command/', views.MQTTCommand),
+    path('api/register', views.UserRegister),
+    path('api/login', views.UserLogin),
+    path('api/logout', views.UserLogout),
+    path('api/mqtt/data', views.MQTTData),
+    path('api/mqtt/data/device', views.MQTTDataDevice),
+    path('api/mqtt/command', views.MQTTCommand),
 ]
