943581597ec1089144b6e176a406fb84f77ebb57
Samuel Tóth <mr.samueltoth@gmail.com>
2024-02-03 22:05:37 +0100

    feat: add storing Bitcoin price in registers and command line parser

--- modbus-server.py
+++ modbus-server.py
@@ -1,72 +1,110 @@
-import random
-import threading
+import os
 import time
+import logging
 import argparse
-import signal
-from pymodbus.datastore import ModbusSequentialDataBlock, ModbusServerContext, ModbusSlaveContext
+import requests
+import threading
+
 from pymodbus.server import StartTcpServer
+from pymodbus.payload import BinaryPayloadBuilder, Endian
+from pymodbus.datastore import (
+    ModbusSequentialDataBlock,
+    ModbusServerContext,
+    ModbusSlaveContext,
+)
+from dotenv import load_dotenv
 
-# Argument parsing for command line options
-parser = argparse.ArgumentParser(description='Modbus server configuration.')
-parser.add_argument('--host', type=str, default='localhost',
-                    help='Host for the Modbus server, default is localhost')
-parser.add_argument('--port', type=int, default=5020,
-                    help='Port for the Modbus server, default is 5020')
-args = parser.parse_args()
-
-# Global variable to control the server running state
-server_running = True
+load_dotenv()
 
 
-def setup_server_context():
-    """
-    Set up the server context for the Modbus server.
+class ModbusServer:
 
-    Returns:
-        ModbusServerContext: The server context with initialized slave context.
-    """
-    initial_values = [0] * 100
-    datablock = ModbusSequentialDataBlock(0, initial_values)
+    def __init__(self, host, port, interval):
+        self.host = host
+        self.port = port
+        self.running = True
+        self.interval = interval
 
-    context = ModbusSlaveContext(
-        di=datablock, co=datablock, hr=datablock, ir=datablock)
-    return ModbusServerContext(slaves=context, single=True)
+        self.setup_server_context()
+
+    def setup_server_context(self):
+        initial_values = [0]
+        datablock = ModbusSequentialDataBlock(0, initial_values)
+
+        slave_context = ModbusSlaveContext(
+            di=datablock, co=datablock, hr=datablock, ir=datablock
+        )
+        self.context = ModbusServerContext(slaves=slave_context, single=True)
+
+    def update_registers(self):
+        while self.running:
+            address = 0
+            price = self.fetch_data()
+
+            builder = BinaryPayloadBuilder(byteorder=Endian.LITTLE)
+            builder.add_32bit_float(float(price))
+            payload = builder.to_registers()
+
+            self.context[0x01].setValues(3, address, payload)
+            logging.info(
+                f"Updated register values at address {address} with {price} and payload {payload}"
+            )
+            time.sleep(int(self.interval))
+
+    def run(self):
+        logging.info(f"Starting Modbus server at {self.host}:{self.port}")
+        StartTcpServer(
+            context=self.context, address=(
+                self.host, self.port), broadcast_enable=True
+        )
+
+    def fetch_data(self):
+        try:
+            response = requests.get(os.getenv("COINCAP_API_URL") + "/bitcoin")
+            data = response.json()
+            price = data.get("data").get("priceUsd")
+            return price
+        except requests.RequestException as e:
+            logging.error(f"Failed to fetch data with error: {e}")
+            return None
 
 
-def update_registers(context):
-    global server_running
-    while server_running:
-        address = 0
-        value = [random.randint(0, 100) for _ in range(100)]
-        context[0x01].setValues(3, address, value)
-        time.sleep(10)
+if __name__ == "__main__":
 
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
 
-def signal_handler(sig, frame):
-    global server_running
-    print('Shutting down server...')
-    server_running = False
-
-
-def main():
-    context = setup_server_context()
-
-    update_thread = threading.Thread(target=update_registers, args=(context,))
-    update_thread.start()
-
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
+    parser = argparse.ArgumentParser(
+        description="Modbus server for fetching and storing Bitcoin price to registers"
+    )
+    parser.add_argument(
+        "--host",
+        type=str,
+        default="localhost",
+        help="Host for the Modbus server, default is localhost",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=5020,
+        help="Port for the Modbus server, default is 5020",
+    )
+    parser.add_argument(
+        "--interval",
+        type=int,
+        default=60,
+        help="Interval in seconds to update register values, default is 60",
+    )
+    args = parser.parse_args()
 
     try:
-        print(f"Starting Modbus server at {args.host}:{args.port}")
-        print("Press Ctrl+C to stop the server")
-        StartTcpServer(context=context, address=(
-            args.host, args.port), broadcast_enable=True)
+        server = ModbusServer(args.host, args.port, args.interval)
+        update_thread = threading.Thread(target=server.update_registers)
+        update_thread.start()
+        server.run()
+    except KeyboardInterrupt:
+        logging.info(f"Modbus server stopped by user.")
     finally:
-        global server_running
-        server_running = False
+        server.running = False
         update_thread.join()
-
-
-if __name__ == '__main__':
-    main()
